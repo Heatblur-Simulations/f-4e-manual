911b8cda60b3056ce5a449aa5a38dae6ef78d423
Daniel Tischner <zabuza.dev@gmail.com>
2023-10-13 12:41:19 +0200

    trying out version number

--- src/conf.py
+++ src/conf.py
@@ -32,7 +32,7 @@ html_theme_options = {
     'canonical_url': '',
     'analytics_id': '',
     'logo_only': False,
-    'display_version': False,
+    'display_version': True,
     'prev_next_buttons_location': 'both',
     'style_external_links': False,
     #'vcs_pageview_mode': '',
